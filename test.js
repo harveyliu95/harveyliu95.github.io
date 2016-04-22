71d84fbcd3492d8422bef698e6c598359b8888a3
liutongl5 <liutongl5@gmail.com>
2016-04-22 22:22:53 +0800

    Added files via upload

--- test.js
+++ test.js
@@ -6,15 +6,12 @@ var myStream;
 var streamSetting = { video: false, audio: true};
 var aTag = document.getElementById('aTag');
 var aContext = new AudioContext();
-var aSrc = window.URL.createObjectURL(aContext.destination);
-aTag.src = aSrc;
 
 var aRecorder;
-var tempBuffer = aContext.createBufferSource();
 var chunks = [];
 
 if (navigator.mediaDevices.getUserMedia) {
-	navigator.mediaDevices.getUserMedia({video:false, audio:true}).then(function(stream) {console.log("success"); mediaSuccess(stream);} ).catch(function(err){mediaFail(err);});
+	navigator.mediaDevices.getUserMedia( streamSetting ).then( mediaSuccess ).catch(function(err){mediaFail(err);});
 } else if (getMedia) {
 	getMedia( streamSetting, mediaSuccess, mediaFail );
 } else {
@@ -27,7 +24,9 @@ function mediaSuccess (stream) {
 	aRecorder.ondataavailable = function(evt) {
 		chunks.push(evt.data);
 		var aBlob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
-		tempBuffer.buffer = aBlob;
+		var temp = window.URL.createObjectURL(aBlob);
+		window.URL.revokeObjectURL(aTag.src);
+		aTag.src = temp;
 		chunks.length = 0;
 }
 
