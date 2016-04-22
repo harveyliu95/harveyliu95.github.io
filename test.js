1230d622e2787cfc8a0e29ba8c39aee60092a37f
liutongl5 <liutongl5@gmail.com>
2016-04-22 20:34:31 +0800

    Added files via upload

---
+++ test.js
@@ -0,0 +1,43 @@
+var getMedia = navigator.getUserMedia ||
+			   navigator.webkitGetUserMedia ||
+			   navigator.mozGetUserMedia ||
+			   navigator.msGetUserMedia ;
+var myStream;
+var streamSetting = { video: false, audio: true};
+var aTag = document.getElementById('aTag');
+var aContext = new AudioContext();
+var aDestNode = aContext.createMediaStreamDestination();
+var aRecorder = new MediaRecorder( aDestNode.stream );
+aRecorder.ondataavailable = function(evt) {
+	chunks.push(evt.data);
+	var aBlob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
+	window.URL.revokeObjectURL(aTag.src);
+	aTag.src = window.URL.createObjectURL(aBlob);
+}
+var chunks = [];
+
+if (navigator.mediaDevices.getUserMedia) {
+	navigator.mediaDevices.getUserMedia(streamSetting).then(function(stream){mediaSuccess(stream);}).catch(mediaFail);
+} else if (getMedia) {
+	getMedia( streamSetting, mediaSuccess, mediaFail );
+} else {
+	console.log("getUserMedia not supported");
+}
+
+var mediaSuccess = function(stream) {
+	console.log("here");
+	myStream = stream;
+	var aSrcNode = aContext.createMediaStreamSource(stream);
+	aSrcNode.connect(aDestNode);
+	beginGetSound();
+};
+
+var mediaFail = function(err) {console.log(err);};
+
+function beginGetSound () {
+	aRecorder.start();
+	setTimeout(function() {
+		aRecorder.stop();
+		beginGetSound();
+	}, 200);
+}
